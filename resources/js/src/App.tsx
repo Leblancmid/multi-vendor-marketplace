fd409c24c18c40fcaa931650787a103f8fadb1c7
“Leblanc” <nabongmichaeladriane@gmail.com>
2026-03-19 10:43:35 +0800

    setup: Laravel + React + sail environment

---
+++ resources/js/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Hello React in Laravel 🚀</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("app")!).render(<App />);
+
+export default App;
