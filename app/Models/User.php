ff1726b7289a3fbeb4beed0ad1ac83ae4f411887
“Leblanc” <nabongmichaeladriane@gmail.com>
2026-03-25 18:26:11 +0800

    cart and cartitem migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Order::class);
     }
+
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
+    }
 }
