5e04ee4378a8713d3e78034090f2e09ac678e55c
“Leblanc” <nabongmichaeladriane@gmail.com>
2026-03-26 11:22:08 +0800

    added route for ProductController

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -9,6 +10,16 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{product:slug}', [ProductController::class, 'show']);
+
+Route::middleware(['auth:sanctum', 'role:vendor'])->group(function () {
+    Route::get('/vendor/products', [ProductController::class, 'vendorIndex']);
+    Route::post('/vendor/products', [ProductController::class, 'store']);
+    Route::put('/vendor/products/{product}', [ProductController::class, 'update']);
+    Route::delete('/vendor/products/{product}', [ProductController::class, 'destroy']);
 });
 
 Route::middleware(['auth:sanctum', 'role:admin'])->get('/admin-only', function () {
